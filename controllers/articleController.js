edc37a87f50f1d91278d88b704b103929c6f2193
dstock3 <dstock3@gmail.com>
2023-09-17 19:53:46 -0500

    created articleController.js

---
+++ controllers/articleController.js
@@ -0,0 +1,56 @@
+const Article = require('../model/article');
+
+exports.createArticle = async (req, res) => {
+    try {
+        const article = new Article(req.body);
+        await article.save();
+        res.status(201).json(article);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+exports.getAllArticles = async (req, res) => {
+    try {
+        const articles = await Article.find();
+        res.json(articles);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+exports.getArticleById = async (req, res) => {
+    try {
+        const article = await Article.findById(req.params.id);
+        if (!article) {
+            return res.status(404).json({ message: 'Article not found' });
+        }
+        res.json(article);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+exports.updateArticle = async (req, res) => {
+    try {
+        const article = await Article.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!article) {
+            return res.status(404).json({ message: 'Article not found' });
+        }
+        res.json(article);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+exports.deleteArticle = async (req, res) => {
+    try {
+        const article = await Article.findByIdAndRemove(req.params.id);
+        if (!article) {
+            return res.status(404).json({ message: 'Article not found' });
+        }
+        res.json({ message: 'Article deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const articleRoutes = require('./routes/articleRoutes');
 require('./database.js');
 
 const app = express();
@@ -8,10 +9,7 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 app.use(morgan('combined'));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/articles', articleRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
