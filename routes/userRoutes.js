87681dd71c599f327904045392c12f48d849f2ef
dstock3 <dstock3@gmail.com>
2023-09-19 15:50:36 -0500

    created auth middleware and implemented in userRoutes.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const { ensureAuthenticated } = require('../middlewares/auth');
 
 router.post('/', userController.createUser);
-router.get('/', userController.getAllUsers);
-router.get('/:userId', userController.getUserById);
-router.get('/username/:username', userController.getUserByUsername);
-router.put('/:userId', userController.updateUser);
-router.delete('/:userId', userController.deleteUser);
+router.get('/', ensureAuthenticated, userController.getAllUsers);
+router.get('/:userId', ensureAuthenticated, userController.getUserById);
+router.get('/username/:username', ensureAuthenticated, userController.getUserByUsername);
+router.put('/:userId', ensureAuthenticated, userController.updateUser);
+router.delete('/:userId', ensureAuthenticated, userController.deleteUser);
 
-router.post('/:userId/contributions', userController.addContribution);
-router.put('/:userId/contributions/:contributionId', userController.updateContribution);
-router.delete('/:userId/contributions/:contributionId', userController.deleteContribution);
+router.post('/:userId/contributions', ensureAuthenticated, userController.addContribution);
+router.put('/:userId/contributions/:contributionId', ensureAuthenticated, userController.updateContribution);
+router.delete('/:userId/contributions/:contributionId', ensureAuthenticated, userController.deleteContribution);
 
 module.exports = router;
