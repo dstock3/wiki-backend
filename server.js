c574a868f4f957c8c797fedc95dc522b91211138
David Stockdale <dstock3@gmail.com>
2023-09-25 17:35:27 -0500

    Added error monitoring and logging mechanisms.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const rateLimit = require("express-rate-limit");
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
 const session = require('express-session');
 const cors = require('cors');
 const passport = require('passport');
-const initializePassport = require('./passport-config');  
+const initializePassport = require('./passport-config');
+const morgan = require('morgan');
+const logger = require('./logger');
 require('dotenv').config();
 
 const articleRoutes = require('./routes/articleRoutes');
@@ -29,7 +30,7 @@ const PORT = 5000;
 const isProduction = process.env.NODE_ENV === 'production';
 
 app.use(bodyParser.json());
-app.use(morgan('combined'));
+app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
 
 app.use(session({
   secret: SECRET,
@@ -59,6 +60,17 @@ app.use('/articles', apiLimiter, articleRoutes);
 app.use('/portals', apiLimiter, portalRoutes);
 app.use('/talk', apiLimiter, talkRoutes);
 app.use('/users', apiLimiter, userRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  logger.error(`${error.status || 500} - ${error.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+  res.status(error.status || 500).send({ error: error.message });
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
