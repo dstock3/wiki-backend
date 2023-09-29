d9461f39bec072a286c978ed66457605430ec140
David Stockdale <dstock3@gmail.com>
2023-09-28 21:02:52 -0500

    revised the order of routes in articleRoutes file.

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -8,12 +8,12 @@ const { articleValidationRules } = require('../validators/articleValidator');
 
 router.get('/', articleController.getAllArticles);
 router.get('/search', articleController.searchArticles);
-router.get('/:articleId', articleController.getArticleById);
 router.get('/:articleId/:sectionId', articleController.getSection);
+router.get('/:articleId', articleController.getArticleById);
 router.post('/', ensureAuthenticated, csrfProtection, ...articleValidationRules, articleController.createArticle);
-router.put('/:articleId', ensureAuthenticated, csrfProtection, ...articleValidationRules, articleController.updateArticle);
-router.delete('/:articleId', ensureAuthenticated, csrfProtection, articleController.deleteArticle);
 router.put('/:articleId/:sectionId', ensureAuthenticated, csrfProtection, articleController.updateSection);
 router.delete('/:articleId/:sectionId', ensureAuthenticated, csrfProtection, articleController.deleteSection);
+router.put('/:articleId', ensureAuthenticated, csrfProtection, ...articleValidationRules, articleController.updateArticle);
+router.delete('/:articleId', ensureAuthenticated, csrfProtection, articleController.deleteArticle);
 
 module.exports = router;
