618a8133beefa557e189f21d3f912461795981ad
David Stockdale <dstock3@gmail.com>
2023-10-03 21:37:00 -0500

    revised the getUserByUsername controller so that hashed password isn't included in response.

--- controllers/userController.js
+++ controllers/userController.js
@@ -84,7 +84,7 @@ exports.getUserById = async (req, res) => {
 
 exports.getUserByUsername = async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.params.username });
+    const user = await User.findOne({ username: req.params.username }, '-password');
     if (!user) return res.status(404).json({ error: 'User not found' });
     res.status(200).json(user);
   } catch (error) {
