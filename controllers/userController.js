2dc851f4249b70ffca8293e07a09571f074f0870
David Stockdale <dstock3@gmail.com>
2023-10-13 17:17:02 -0500

    revised get user controllers to return info related to whether user is viewing their own info.

--- controllers/userController.js
+++ controllers/userController.js
@@ -76,7 +76,10 @@ exports.getUserById = async (req, res) => {
   try {
     const user = await User.findById(req.params.userId);
     if (!user) return res.status(404).json({ error: 'User not found' });
-    res.status(200).json(user);
+
+    const isOwnProfile = req.session.userId === req.params.userId;
+
+    res.status(200).json({ user, isOwnProfile });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -86,7 +89,10 @@ exports.getUserByUsername = async (req, res) => {
   try {
     const user = await User.findOne({ username: req.params.username }, '-password');
     if (!user) return res.status(404).json({ error: 'User not found' });
-    res.status(200).json(user);
+
+    const isOwnProfile = req.session.userId === req.params.userId;
+
+    res.status(200).json({ user, isOwnProfile });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
