519f21f7b590daf9f503507b304463fba11ba392
dstock3 <dstock3@gmail.com>
2023-11-12 00:19:09 -0600

    implemented logging within auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,20 @@
+const logger = require('../logger');
+
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
+    logger.info(`Unauthorized access attempt from ${req.ip}: ${req.method} ${req.originalUrl}`);
     res.status(401).json({ error: 'Unauthorized' });
 }
 
 function ensureAdmin(req, res, next) {
     if (req.user && req.user.isAdmin) {
+        logger.info(`Admin access granted to user ${req.user.username}: ${req.method} ${req.originalUrl}`);
         return next();
     }
-    res.status(401).json({ error: 'Unauthorized' });
+    logger.warn(`Admin access denied to user ${req.user ? req.user.username : 'unknown'} from IP ${req.ip}: ${req.method} ${req.originalUrl}`);
+    res.status(401).json({ error: 'Access denied. Admin privileges required.' });
 }
   
 module.exports = {
