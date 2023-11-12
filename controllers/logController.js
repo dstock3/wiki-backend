92a5280b09a80c824a5f8693cc836f17a0f3560b
dstock3 <dstock3@gmail.com>
2023-11-12 02:14:58 -0600

    created log parsing function to ensure logs are always sent as an object in the response.

--- controllers/logController.js
+++ controllers/logController.js
@@ -1,10 +1,23 @@
 const logService = require('../services/logService');
 const logger = require('../logger');
 
+const parseLogs = logs => {
+    return logs.map(log => {
+        try {
+            return JSON.parse(log);
+        }
+        catch (error) {
+            logger.error(`Error parsing log entry: ${error.message}`);
+            return log;
+        }
+    });
+};
+
 exports.getAllLogs = async (req, res) => {
     try {
         const logs = await logService.fetchAllLogs();
-        res.status(200).json({ logs });
+        const parsedLogs = parseLogs(logs);
+        res.status(200).json({ logs: parsedLogs });
     } catch (error) {
         logger.error(`Error fetching all logs: ${error.message}`);
         res.status(500).json({ error: error.message });
@@ -14,7 +27,8 @@ exports.getAllLogs = async (req, res) => {
 exports.getAllErrorLogs = async (req, res) => {
     try {
         const errorLogs = await logService.fetchLogsByLevel('error');
-        res.status(200).json({ errorLogs });
+        const parsedLogs = parseLogs(errorLogs);
+        res.status(200).json({ errorLogs: parsedLogs });
     } catch (error) {
         logger.error(`Error fetching error logs: ${error.message}`);
         res.status(500).json({ error: error.message });
@@ -24,7 +38,8 @@ exports.getAllErrorLogs = async (req, res) => {
 exports.getAllInfoLogs = async (req, res) => {
     try {
         const infoLogs = await logService.fetchLogsByLevel('info');
-        res.status(200).json({ infoLogs });
+        const parsedLogs = parseLogs(infoLogs);
+        res.status(200).json({ infoLogs: parsedLogs });
     } catch (error) {
         logger.error(`Error fetching info logs: ${error.message}`);
         res.status(500).json({ error: error.message });
@@ -34,7 +49,8 @@ exports.getAllInfoLogs = async (req, res) => {
 exports.getAllWarnLogs = async (req, res) => {
     try {
         const warnLogs = await logService.fetchLogsByLevel('warn');
-        res.status(200).json({ warnLogs });
+        const parsedLogs = parseLogs(warnLogs);
+        res.status(200).json({ warnLogs: parsedLogs });
     } catch (error) {
         logger.error(`Error fetching warn logs: ${error.message}`);
         res.status(500).json({ error: error.message });
@@ -45,7 +61,8 @@ exports.searchLogs = async (req, res) => {
     const { query } = req.query;
     try {
         const searchResults = await logService.searchLogs(query);
-        res.status(200).json({ searchResults });
+        const parsedResults = parseLogs(searchResults);
+        res.status(200).json({ searchResults: parsedResults });
     } catch (error) {
         logger.error(`Error searching logs: ${error.message}`);
         res.status(500).json({ error: error.message });
